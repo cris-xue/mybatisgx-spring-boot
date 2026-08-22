4c6e29d6a74f892f5851942d2d90b252bc236e61
薛承城 <xcc137396549@foxmail.com>
2026-08-22 23:18:03 +0800

    [FIX] 升级mp依赖，解决mp无法分页问题

--- mybatisgx-spring-boot2-mp-compat-starter/src/main/java/com/mybatisgx/boot/MybatisgxConfiguration.java
+++ mybatisgx-spring-boot2-mp-compat-starter/src/main/java/com/mybatisgx/boot/MybatisgxConfiguration.java
@@ -1,14 +1,47 @@
 package com.mybatisgx.boot;
 
-import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.autoconfigure.SqlSessionFactoryBeanCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.github.pagehelper.PageInterceptor;
 import com.mybatisgx.ext.scripting.xmltags.MgxsqlLanguageDriver;
+import com.mybatisgx.mybatisplus.MybatisgxPlusConfiguration;
 import org.apache.ibatis.type.TypeAliasRegistry;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 public class MybatisgxConfiguration {
+
+    private final List<ConfigurationCustomizer> configurationCustomizers;
+
+    public MybatisgxConfiguration(ObjectProvider<List<ConfigurationCustomizer>> configurationCustomizersProvider) {
+        this.configurationCustomizers = configurationCustomizersProvider.getIfAvailable();
+    }
+
+    @Bean
+    public SqlSessionFactoryBeanCustomizer mybatisgxPlusSqlSessionFactoryBeanCustomizer(MybatisgxProperties mybatisgxProperties) {
+        return factoryBean -> {
+            MybatisgxPlusConfiguration mybatisgxPlusConfiguration = new MybatisgxPlusConfiguration();
+            // 未配置 mybatisgx.configuration.* 时 CoreConfiguration 为 null（MP 3.5.17 不预初始化），需空判断
+            MybatisgxProperties.CoreConfiguration coreConfiguration = mybatisgxProperties.getConfiguration();
+            if (coreConfiguration != null) {
+                coreConfiguration.applyTo(mybatisgxPlusConfiguration);
+            }
+            if (!CollectionUtils.isEmpty(this.configurationCustomizers)) {
+                for (ConfigurationCustomizer customizer : this.configurationCustomizers) {
+                    customizer.customize(mybatisgxPlusConfiguration);
+                }
+            }
+            TypeAliasRegistry typeAliasRegistry = mybatisgxPlusConfiguration.getTypeAliasRegistry();
+            typeAliasRegistry.registerAlias("mgxsql", MgxsqlLanguageDriver.class);
+            factoryBean.setConfiguration(mybatisgxPlusConfiguration);
+        };
+    }
 
     @Bean
     public PageInterceptor pageInterceptor() {
@@ -16,17 +49,15 @@ public class MybatisgxConfiguration {
     }
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer customizer() {
-        return builder -> builder.mixIn(Object.class, IgnoreHandlerMixin.class);
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
     }
 
     @Bean
-    public MybatisPlusPropertiesCustomizer mybatisgxPlusConfigurationCustomizer() {
-        return mybatisPlusProperties -> {
-            MybatisConfiguration mybatisConfiguration = mybatisPlusProperties.getConfiguration();
-            TypeAliasRegistry typeAliasRegistry = mybatisConfiguration.getTypeAliasRegistry();
-            typeAliasRegistry.registerAlias("mgxsql", MgxsqlLanguageDriver.class);
-        };
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return builder -> builder.mixIn(Object.class, IgnoreHandlerMixin.class);
     }
 
     /**
--- mybatisgx-spring-boot2-mp-compat-starter/src/main/java/com/mybatisgx/boot/MybatisgxProperties.java
+++ mybatisgx-spring-boot2-mp-compat-starter/src/main/java/com/mybatisgx/boot/MybatisgxProperties.java
@@ -1,10 +1,7 @@
 package com.mybatisgx.boot;
 
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
-import com.mybatisgx.mybatisplus.MybatisgxPlusConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 /**
  * MybatisxProperties
@@ -13,18 +10,4 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 public class MybatisgxProperties extends MybatisPlusProperties {
 
     public static final String MYBATIS_PREFIX = "mybatisgx";
-
-    @NestedConfigurationProperty
-    private MybatisgxPlusConfiguration configuration = new MybatisgxPlusConfiguration();
-
-    @Override
-    public MybatisConfiguration getConfiguration() {
-        return configuration;
-    }
-
-    public MybatisPlusProperties setConfiguration(MybatisConfiguration configuration) {
-        this.configuration = (MybatisgxPlusConfiguration) configuration;
-        super.setConfiguration(configuration);
-        return this;
-    }
 }
