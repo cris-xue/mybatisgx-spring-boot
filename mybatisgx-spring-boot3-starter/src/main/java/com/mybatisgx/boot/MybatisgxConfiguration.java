a3466589d798a56d4eeee17ff0002b252451c9ad
薛承城 <xcc137396549@foxmail.com>
2026-08-28 20:36:24 +0800

    [REM] 删除废弃代码和修改依赖

--- mybatisgx-spring-boot3-starter/src/main/java/com/mybatisgx/boot/MybatisgxConfiguration.java
+++ mybatisgx-spring-boot3-starter/src/main/java/com/mybatisgx/boot/MybatisgxConfiguration.java
@@ -2,7 +2,6 @@ package com.mybatisgx.boot;
 
 import com.github.pagehelper.PageInterceptor;
 import com.mybatisgx.ext.scripting.xmltags.MgxsqlLanguageDriver;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.type.TypeAliasRegistry;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
@@ -11,9 +10,7 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.ReflectionUtils;
 
-import java.lang.reflect.Modifier;
 import java.util.List;
 
 public class MybatisgxConfiguration {
@@ -38,28 +35,10 @@ public class MybatisgxConfiguration {
                     customizer.customize(mybatisgxConfiguration);
                 }
             }
-
-            /*Field field = ReflectionUtils.findField(SqlSessionFactoryBean.class, "configuration");
-            field.setAccessible(true);
-            Configuration configuration = (Configuration) ReflectionUtils.getField(field, factoryBean);
-            com.mybatisgx.ext.session.MybatisgxConfiguration mybatisgxConfiguration = this.copyNonFinalFields(configuration);*/
             TypeAliasRegistry typeAliasRegistry = mybatisgxConfiguration.getTypeAliasRegistry();
             typeAliasRegistry.registerAlias("mgxsql", MgxsqlLanguageDriver.class);
             factoryBean.setConfiguration(mybatisgxConfiguration);
         };
-    }
-
-    private com.mybatisgx.ext.session.MybatisgxConfiguration copyNonFinalFields(Configuration source) {
-        com.mybatisgx.ext.session.MybatisgxConfiguration mybatisgxConfiguration = new com.mybatisgx.ext.session.MybatisgxConfiguration();
-        ReflectionUtils.doWithFields(Configuration.class, field -> {
-            if (Modifier.isFinal(field.getModifiers()) || Modifier.isStatic(field.getModifiers())) {
-                return;
-            }
-            ReflectionUtils.makeAccessible(field);
-            Object value = ReflectionUtils.getField(field, source);
-            ReflectionUtils.setField(field, mybatisgxConfiguration, value);
-        });
-        return mybatisgxConfiguration;
     }
 
     @Bean
